2efaa69dacca71bec957baf18e341a85d7010c1d
Denis-us <75166604+Denis-us@users.noreply.github.com>
2022-06-14 22:24:53 +0300

    Ще одна

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react'
-import {store, persistor} from './redux/store';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/store';
 import "./index.css";
 import App from "./App";
 
@@ -11,11 +11,11 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={'react-hw-07-phonebook'}>
+        <BrowserRouter basename={'react-hw-08-phonebook'}>
           <App />
         </BrowserRouter>
       </PersistGate>
-    </Provider> 
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
